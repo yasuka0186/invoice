5e7999f5a0b74e8286aadfbd71c3e94d126a8542
yasuka0186 <yasuka0186@icloud.com>
2026-03-15 18:20:32 +0900

    追加の初期設定

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,12 @@ import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
 
+/**
+ * Vueアプリケーションの初期化処理
+ * - Pinia を登録
+ * - Router を登録
+ * - App.vue をマウント
+ */
 const app = createApp(App)
 
 app.use(createPinia())
